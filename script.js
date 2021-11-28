3b6269ce83f3a62b1f317a6f81c8b1cc9604ef8e
ashleighpresley <ashleighpresley1@gmail.com>
2021-11-28 16:30:44 -0600

    Validate form inputs

--- script.js
+++ script.js
@@ -100,14 +100,44 @@ class App {
 
   //create new marker and add it to the map
   _newWorkout(e) {
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+    const allPositive = (...inputs) => inputs.every(inp => inp >= 0);
+
     e.preventDefault();
 
-    //clear input fields
-    inputDistance.value =
-      inputDuration.value =
-      inputCadence.value =
-      inputElevation.value =
-        '';
+    //get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+
+    //if activity is running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      //check if data is valid
+      if (
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      ) {
+        return alert('Inputs have to be postive numbers!');
+      }
+    }
+
+    //if activity is cycling, create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      //check if data is valid
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be postive numbers!');
+    }
+
+    //add new object to workout array
+
+    //render workout on map at marker
+
     const { lat, lng } = this.#mapEvent.latlng;
 
     L.marker([lat, lng])
@@ -123,6 +153,15 @@ class App {
       )
       .setPopupContent('Workout')
       .openPopup();
+
+    //render workout on list
+
+    //hide form + clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
   }
 }
 
